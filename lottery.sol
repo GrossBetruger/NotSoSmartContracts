d4ee634dfd93c06d66ba0c8afed1432e65dd40e7
Oren Koriat <oren@cynerio.co>
2018-07-25 16:31:59 +0300

    changed game to choose a random name from contestants

--- lottery.sol
+++ lottery.sol
@@ -2,21 +2,28 @@ pragma solidity ^0.4.0;
 
 contract Lottery {
     
+    mapping (uint8 => string) private nameMap;
+    
+    
     constructor () public {
-        
+        nameMap[0] = "oren";
+        nameMap[1] = "yuval";
+        nameMap[2] = "gilad";
+        nameMap[3] = "shnior";
+        nameMap[4] = "sluk";
+        nameMap[5] = "jojo";
+  
     }
     
     function random() private view returns (uint8) {
-        return uint8(uint256(keccak256(block.timestamp, block.difficulty))%200);
+        return uint8(uint256(keccak256(block.timestamp, block.difficulty))%6);
     }
     
     function play() public constant returns(string) {
         
         uint8 result = random();
-        if (result < 100) {
-            return "you win!";
-        }
-        return  "you lose...";
+        return nameMap[result];
+  
     }
     
 }
