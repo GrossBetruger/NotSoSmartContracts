b39b83d5d0fee49b20a3c3b0265d2996168dd694
koi <koriatoren@gmail.com>
2018-07-27 22:55:00 +0300

    security fix: only owner of the contract can execute the game

--- lottery.sol
+++ lottery.sol
@@ -2,10 +2,16 @@ pragma solidity ^0.4.0;
 
 contract Lottery {
     
+    address owner; 
+    
     mapping (uint8 => string) private nameMap;
     
+    modifier OnlyOwner { require(msg.sender == owner); _; } 
     
     constructor () public {
+        
+        owner = msg.sender;
+        
         nameMap[0] = "oren";
         nameMap[1] = "yuval";
         nameMap[2] = "gilad";
@@ -19,7 +25,7 @@ contract Lottery {
         return uint8(uint256(keccak256(block.timestamp, block.difficulty))%6);
     }
     
-    function play() public constant returns(string) {
+    function play() public OnlyOwner constant returns(string) {
         
         uint8 result = random();
         return nameMap[result];
